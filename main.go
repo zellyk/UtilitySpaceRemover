9715d484b112b3106cea423196fa721fc88f8fc8
Jonathan Marcotte <jonathan.marcotte.7@gmail.com>
2023-09-14 11:13:26 -0400

    One commit only, quick space remover for file names in Go

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	folderPath := "/Users/neotokyo/Desktop/Photos Bains SériePRO 2"
+
+	files, err := ioutil.ReadDir(folderPath)
+	if err != nil {
+		fmt.Println("Error reading directory:", err)
+		return
+	}
+
+	for _, file := range files {
+		oldName := file.Name()
+		newName := strings.ReplaceAll(oldName, " ", "") // Remove spaces from the file name
+
+		if oldName != newName {
+			oldPath := filepath.Join(folderPath, oldName)
+			newPath := filepath.Join(folderPath, newName)
+
+			if err := os.Rename(oldPath, newPath); err != nil {
+				fmt.Printf("Error renaming %s to %s: %v\n", oldPath, newPath, err)
+			} else {
+				fmt.Printf("Renamed %s to %s\n", oldPath, newPath)
+			}
+		}
+	}
+}
